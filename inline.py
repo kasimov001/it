403becd29a3b64f2dfed14b925d7c1a3b4f00d5a
kasimov001 <116807259+kasimov001@users.noreply.github.com>
2022-11-22 18:25:29 +0500

    Add files via upload

---
+++ inline.py
@@ -0,0 +1,147 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+menuTosh = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="t1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="t2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+
+menuNam = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="n1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="n2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuAnd = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="a1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="a2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuFarg = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="f1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="f2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuBux = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="b2"),
+            InlineKeyboardButton(text="Ertaga", callback_data="b3")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuGul = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="g1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="g2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuXiva = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="x1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="x2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuSam = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="s1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="s2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuNav = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="m1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="m2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuJizzakh = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="j1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="j2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuNuk = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="o1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="o2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
+menuQar = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Bugun", callback_data="q1"),
+            InlineKeyboardButton(text="Ertaga", callback_data="q2")
+        ],
+        [
+            InlineKeyboardButton(text="🔙 Ortga", callback_data="b1"),
+            InlineKeyboardButton(text="Menu", callback_data="menu")
+        ]
+    ]
+)
